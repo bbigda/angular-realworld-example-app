3acd3b2d1d477e4f0f22fbb7cf40329edd529290
Bartłomiej Bigda <bartlomiej.bigda@promity.pl>
2022-03-04 07:56:32 +0100

    pre commit tslint

--- src/app/shared/cat-facts/catFact.ts
+++ src/app/shared/cat-facts/catFact.ts
@@ -1,16 +1,18 @@
 export interface CatFact {
-  _id:       string;
-  status:    CatFactStatus;
-  type:      string;
-  deleted:   boolean;
-  user:      string;
-  text:      string;
+  _id: string;
+  status: CatFactStatus;
+  type: string;
+  deleted: boolean;
+  user: string;
+  text: string;
   createdAt: string;
   updatedAt: string;
-  __v:       number;
+  source: string;
+  used: boolean;
+  __v: number;
 }
 
 export interface CatFactStatus {
-  verified:  boolean | null;
+  verified: boolean | null;
   sentCount: number;
 }
